ba8bf76129a6542289f66e5d93e0621d64f4b768
Artem Shpakov <artshpakov@yandex.ru>
2015-05-27 00:49:31 +0300

    Battle announcements

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -8,13 +8,22 @@ class Battle < ActiveRecord::Base
   scope :open, -> { where("open_until > ?", Time.now) }
   scope :ordered, -> { order("(open_until > '#{ Time.now }') DESC").order(created_at: :desc) }
 
+  after_create :announce
+
 
   def open?
     open_until > Time.now
   end
 
   def last_entry
-    entries.order(created_at: :desc).limit(1).first
+    entries.order(id: :desc).limit(1).first
+  end
+
+
+  protected
+
+  def announce
+    corner.posts.create body: "New battle #{ name } announced!"
   end
 
 end
