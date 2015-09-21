0f9a13a299dbc8e0e3e618ca1052b3251fe9aa0f
Artyom Shpakov <artshpakov@yandex.ru>
2015-09-21 14:12:58 +0300

    Timeline for everyone

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,10 +1,14 @@
 class IndexController < ApplicationController
 
+  POSTS_PER_PAGE = 10
+
   def index
     @corners = Corner.populated.limit(30)
-    if current_user
+    @timeline = if current_user
       @followed_corners = current_user.follows
-      @timeline         = Post.where(corner_id: @followed_corners.map(&:id)).limit(10)
+      Post.where(corner_id: @followed_corners.map(&:id)).limit(POSTS_PER_PAGE)
+    else
+      Post.limit(POSTS_PER_PAGE)
     end
   end
 
